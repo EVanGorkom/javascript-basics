4a8fe896d166f9a72cbd18715e3fc672478598f2
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-11 11:55:04 -0600

    Halfway through the basic for_loop practice

---
+++ for_loops.js
@@ -0,0 +1,24 @@
+// for ([initialization]; [condition]; [final-expression]) {
+//   [statement]
+// }
+
+// for (var i = 0; i < 4; i++) {
+//   console.log(i);
+// }
+
+// // Challenge #1: Write code that will print out the numbers 12-18.
+// for (var i = 12; i < 19; i++) {
+//   console.log(i)
+// }
+
+// // Challenge #2:Write code that will print out the numbers 0-5, and use String interpolation to combine the number with an statement of your choice.
+// for (var i = 0; i < 6; i++) {
+//   console.log(i + " and counting")
+// }
+
+// var fruits = ['apples', 'oranges', 'bananas'];
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log("I have some " + fruits[i]);
+// }
+
+// Print only the numbers from the following Array that are greater than 10:
