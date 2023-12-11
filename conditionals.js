a7f575537719481a806768328dc33b37bb21c6f3
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-11 11:03:47 -0600

    Finish the conditionals exercise

---
+++ conditionals.js
@@ -0,0 +1,75 @@
+// if (3 <= 6) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if ("T" === "t") {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if ("P" === "P") {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if (10 != 10) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// var name = "Ethan Van G"
+// console.log(name.length)
+// if (name.length >= 10) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// userAge = 6
+// if (userAge >= 21) {
+//   console.log("Welcome to the site!")
+// } else if (userAge < 5) {
+//   console.log("How are you even on the computer??")
+// } else if (userAge < 21) {
+//   console.log("Please come back when you're older.")
+// } else {
+//   console.log("Invalid age, please try again.")
+// }
+
+
+// ethanAge = 28
+// sarahAge = 26
+// if (ethanAge < 100 && sarahAge < 100) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if (ethanAge < 78 || sarahAge < 78) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if (ethanAge > 18 && sarahAge < 60) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if (ethanAge > 21 || sarahAge > 25) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if (ethanAge < 20 || sarahAge > 40) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
