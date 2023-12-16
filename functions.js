70e5c47898a8321dedd37957881b8897ecf3cd97
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-16 12:21:40 -0600

    Finish the functions lessons for FE mod 0

---
+++ functions.js
@@ -0,0 +1,16 @@
+// function greetATexan() {
+//   return "Howdy, partner!";
+// }
+// console.log(greetATexan())
+
+// Write a function named getAge. This method should declare one parameter, a Number, representing a birth year.
+// When called, the function should calculate the person's approximate age. Let's not worry about birth month or day. 😉
+// The function should return a Number representing the person's age.
+// Call the function several times with different arguments and store the return values in variables.
+// Print a sentence that says, "You have lived ___ years of life!" with each variable interpolated into the String.
+
+// function getAge(birthYear) {
+//   age = 2023 - birthYear
+//   console.log("You have lived " + age + " years of life!")
+// }
+// getAge(1995);
