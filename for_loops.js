307df9f8c016c1b00a3cbfab071979d05f2147f4
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-15 23:58:28 -0600

    Finish the For loop intro lesson

--- for_loops.js
+++ for_loops.js
@@ -16,9 +16,57 @@
 //   console.log(i + " and counting")
 // }
 
+
 // var fruits = ['apples', 'oranges', 'bananas'];
 // for (var i = 0; i < fruits.length; i++) {
 //   console.log("I have some " + fruits[i]);
 // }
 
+
 // Print only the numbers from the following Array that are greater than 10:
+// var numbers = [10, 11, 7, 19, 4, 52, 89, 9, 12, 10]
+// for (i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > 10) {
+//     console.log(numbers[i])
+//   }
+// }
+
+
+// Utilize a for statement to print out the numbers 3-14.
+// for (i = 3; i < 15; i++) {
+//  console.log(i)
+// }
+
+
+// Print the square of each number:
+// var numbers = [2, 4, 6, 8, 10, 12]
+// for (i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i] * numbers[i])
+// }
+
+
+// Print each String, but make each String all lowercase:
+// var words = ["sUnNy", "BeAcH", "wAvEs", "ReLaX"]
+// for (i = 0; i < words.length; i++) {
+//   console.log("" + words[i].toLowerCase())
+// }
+
+
+// Print out only names that begin with "P":
+// var names = ["Pilar", "Petunia", "Pamela", "Tan", "Amanda", "Phil"]
+// for (i = 0; i < names.length; i++) {
+//   if (names[i][0] == "P") {
+//     console.log(names[i])
+//   }
+// }
+
+
+// Print out every odd number between 0 and 100. 
+// Do NOT print any even numbers, and do NOT create an Array to help you do this.
+// for (i = 0; i < 100; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+
